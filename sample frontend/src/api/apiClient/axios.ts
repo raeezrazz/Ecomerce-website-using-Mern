4bd0de6b52f201880e928a8572aab88147f85ee4
raeezrazz <mhdrahees67@gmail.com>
2025-12-04 03:03:15 +0530

    changed the api client

--- sample frontend/src/api/apiClient/axios.ts
+++ sample frontend/src/api/apiClient/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:4000',
+    baseURL: 'https://ecomerce-website-using-mern.onrender.com',
     withCredentials: true
 })
 
