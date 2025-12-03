0570f9df77e2501bd0cab58d15ef3244b3c28516
raeezrazz <mhdrahees67@gmail.com>
2025-12-04 03:06:20 +0530

    changed the cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:8080', 'http://localhost:3000'],
+  origin: ['https://ecomerce-website-using-mern.onrender.com'],
   credentials: true
 }));
 
